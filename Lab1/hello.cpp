ddfb34afd6b6a363db1401fbcda1b59e1e83af67
mariec-j <mcjardiniano@cpp.edu>
2025-08-23 14:24:58 -0700

    Create hello.cpp

---
+++ Lab1/hello.cpp
@@ -0,0 +1,20 @@
+/* 
+Coding Assignment 1
+Using cout and printf it should just output:
+////////
+Hello World!
+Hello World again!
+////////
+
+this is a random comment
+*/
+
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+}
