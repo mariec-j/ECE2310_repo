d7925df2139adf66436ca10585123a9b2fe0e0a6
mariec-j <mcjardiniano@cpp.edu>
2025-08-26 15:27:58 -0700

    Create hello.cpp

---
+++ Lab01/hello.cpp
@@ -0,0 +1,25 @@
+/* 
+Coding Assignment 1
+
+ECE2310 Fall 2025
+Lab 1
+Marie Jardiniano
+August 26, 2025
+
+
+Using cout and printf it should just output:
+--------------------
+Hello World!
+Hello World again!
+--------------------
+cel*/
+
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n\n");
+}
