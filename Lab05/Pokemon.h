ca06d82b26b750cb3fe33c1a55018a4128a2b7fc
mariec-j <mcjardiniano@cpp.edu>
2025-10-22 22:55:43 -0700

    Starting Lab5
    i start the assignment

---
+++ Lab05/Pokemon.h
@@ -0,0 +1,26 @@
+#ifndef POKEMON_H
+#define POKEMON_H
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class Pokemon {
+    public:
+    //              CONSTRUCTORS 
+    Pokemon();
+    Pokemon(string name, int hp, int att, int def, vector<string> type);
+
+    //              MUTATORS
+    virtual void speak();
+    virtual void printStats();
+
+    //              ACCESSORS
+
+    private:
+
+
+
+}
+
+#endif
